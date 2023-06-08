74df71bb27abf279c4125b5e22120a68d7c77e29
Sagar Nahak <sfsagar100@gmail.com>
2023-06-09 01:57:40 +0530

    first commit

--- lwc/helloSetup/helloSetup.js
+++ lwc/helloSetup/helloSetup.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloSetup extends LightningElement {}
+export default class HelloSetup extends LightningElement {
+    // testing github
+}
